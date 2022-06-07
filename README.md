a86ee7b265d38e0978fe9deab165cd9b7e52928a
Rohitsuresh1 <99383088+Rohitsuresh1@users.noreply.github.com>
2022-06-07 11:15:22 -0400

    fixed html display issues

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Team-Profile-Generator
-A node application that creates a dynamic html page from terminal input 
+
+![License:MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+# Team Profile Generator
+           
+## Description
+This project generates a dynamically created HTML page based on the user input into the terminal. It showcases the use of Object Oriented Programming and testing methodologies using jest framework.
+ 
+## Table of Contents
+- [Installation](#inst)
+ 
+- [License](#license)
+- [How to Contribute](#contributing)
+ 
+- [Tests](#tests)
+- [Questions](#questions)
+ 
+
+  
+<a name="inst"></a>
+## Installation
+The program can be cloned from this repository and run by running the command 'node index'. You can also test the program by running 'npm run test'
+
+<a name="license"></a>
+## License
+This application is covered under MIT licensing.
+
+<a name="contributing"></a>
+## How to contribute
+You can contribute to the project by pushing your code to develop branch for review.
+  
+
+<a name="tests"></a>
+## Tests
+This project uses jest framework to test object creation. The tests will all pass if you run the command 'npm run test'
+  
+<a name="questions"></a>
+## Questions
+For any questions you can contact me at:
+email: rohitsuresh1@hotmail.com
+username: rohitsuresh1 GitHub: https://github.com/rohitsuresh1
+  
--- src/pagetemplate.js
+++ src/pagetemplate.js
@@ -3,11 +3,47 @@ var html="";
 const returnHtml=data=>{
     for(i=0;i<data.length;i++){
         if(data[i].getRole()==='Manager'){
-            html+='Manager';
+            html+=`
+            <div class="col-lg-3 mb-2">
+            <div class="card-body bg-danger">
+            <h5 class="card-title">${data[i].getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${data[i].getRole()}</h6>
+            </div>
+            <div class = "card-body bg-secondary">
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${data[i].getId()}</li>
+            <li class="list-group-item">Email: <a href = "mailto:${data[i].email}">${data[i].email}</a></li>
+            <li class="list-group-item">Office Number: ${data[i].officeNumber}</li>
+            </ul>
+            </div></div>`;
         } else if (data[i].getRole()==='Engineer') {
-            html+='Engineer';
+            html+=`
+            <div class="col-lg-3 mb-2">
+            <div class="card-body bg-info">
+            <h5 class="card-title">${data[i].name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${data[i].getRole()}</h6>
+            </div>
+            <div class = "card-body bg-secondary">
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${data[i].id}</li>
+            <li class="list-group-item">Email: <a href = "mailto:${data[i].email}">${data[i].email}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${data[i].github}" target="_blank">${data[i].github}</a></li>
+            </ul>
+            </div></div>`;
         } else {
-            html+='Intern';
+            html+=`
+            <div class="col-lg-3 mb-2">
+            <div class="card-body bg-warning">
+            <h5 class="card-title">${data[i].name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${data[i].getRole()}</h6>
+            </div>
+            <div class = "card-body bg-secondary">
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${data[i].id}</li>
+            <li class="list-group-item">Email: <a href = "mailto:${data[i].email}">${data[i].email}</a></li>
+            <li class="list-group-item">School: ${data[i].school}</li>
+            </ul>
+            </div></div>`;
         }
     }
  return html;
@@ -25,12 +61,16 @@ return `<!doctype html>
         <link rel="stylesheet" href="../dist/style.css" />
     </head>
     <body>
-    <header class="container-fluid text-center text-white">
+    <header class="page-header text-center bg-success">
         <h1>Best Team Ever!</h1>
     </header>
-    <div class="card-deck">
+    
+    <div class="row">
+    <div class="card-deck mx-auto" >
         ${returnHtml(data)}
     </div>
+    </div>
+   
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
